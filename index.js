b1cbf2a972c01528748776c4a1a05e5a3ab3d74d
Nauj Solrac <nauj1solrac@hotmail.com>
2023-11-03 21:04:59 -0400

    Fin seccion 10 - Auth

--- index.js
+++ index.js
@@ -11,19 +11,18 @@ const app = express();
 // Configurar CORS
 app.use(cors());
 
+// Lectura y parseo del Body
+app.use(express.json());
+
 // Base de datos
 dbConnection();
 
 // console.log(process.env);
 
 // Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    })
-})
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto', 3000);
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 })
